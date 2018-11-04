7ffaa37d380323acedab48878858b0d5998fa9c9
Acvarium <vshmorgun@yandex.ru>
2018-11-04 03:27:30 +0200

    Improved movement

--- game/objects/player.gd
+++ game/objects/player.gd
@@ -6,7 +6,7 @@ var is_moving = false
 var on_the_ladder = false
 var main_node
 var speed = 0
-var max_speed = 150
+var max_speed = 200
 var velocity = Vector2()
 enum ENTITY_TYPES {UP, DOWN, LEFT, RIGHT}
 
@@ -21,30 +21,39 @@ func _ready():
 	$rays/right.add_exception(self)
 
 func _physics_process(delta):
+	$rays/up.force_raycast_update()
+	$rays/down.force_raycast_update()
+	$rays/left.force_raycast_update()
+	$rays/right.force_raycast_update()
+	
 	var t = main_node.get_node("ui/dir")
 	var tt = "UP " + str(obstacle(UP)) + " DOWN " + str(obstacle(DOWN))
 	t.text = tt
 	direction = Vector2()
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up") and on_the_ladder:
 		if !obstacle(UP):
 			direction.y = -1
 		if !is_moving:
 			currentDir = Vector2(0,-1)
-	elif Input.is_action_pressed("ui_down"):
+	elif Input.is_action_pressed("ui_down") and on_the_ladder:
 		if !obstacle(DOWN):
 			direction.y = 1
 		if !is_moving:
 			currentDir = Vector2(0,1)
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_left") and (obstacle(DOWN) or on_the_ladder):
 		if !obstacle(LEFT):
 			direction.x = -1
 		if !is_moving:
 			currentDir = Vector2(-1,0)
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("ui_right") and (obstacle(DOWN) or on_the_ladder):
 		if !obstacle(RIGHT):
 			direction.x = 1
 		if !is_moving:
 			currentDir = Vector2(1,0)
+	elif !on_the_ladder and !obstacle(DOWN):
+		direction.y = 1
+		if !is_moving:
+			currentDir = Vector2(0,1)
 
 	if !is_moving and direction != Vector2():
 		print(main_node.is_cell_vacant(self))
@@ -63,18 +72,23 @@ func _physics_process(delta):
 		if abs(velocity.y) > distance_to_target.y:
 			velocity.y = distance_to_target.y * target_direction.y
 			is_moving = false
+		
 		move_and_collide(velocity)
+		if velocity.x < -0.1:
+			$Sprite.scale.x = -0.5
+		elif velocity.x > 0.1:
+			$Sprite.scale.x = 0.5
 
 
 func obstacle(dir):
 	if dir == UP:
-		return $rays/up.is_colliding()
+		return $rays/up.is_colliding() or $rays/up2.is_colliding() 
 	elif dir == DOWN:
-		return $rays/down.is_colliding() 
+		return $rays/down.is_colliding() or $rays/down2.is_colliding() 
 	elif dir == LEFT:
-		return $rays/left.is_colliding()
+		return $rays/left.is_colliding() or $rays/left2.is_colliding()
 	elif dir == RIGHT:
-		return $rays/right.is_colliding()
+		return $rays/right.is_colliding() or $rays/right2.is_colliding()
 
 func _on_Area2D_body_entered(body):
 	print("in")
