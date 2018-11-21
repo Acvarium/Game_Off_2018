dd6f169e62047f5028de649ec39b4a74bfd13e8d
Acvarium <vshmorgun@yandex.ru>
2018-11-21 22:57:59 +0200

    Change pages

--- game/scripts/menu.gd
+++ game/scripts/menu.gd
@@ -1,6 +1,24 @@
 extends Control
 var global
+var page = 0
 
 func _ready():
 	global = get_node("/root/global")
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) 
+	set_page(0)
+
+func set_page(_page):
+	page = _page
+	if page < 0:
+		page = 0
+	$l_cont/page_minus.visible = page > 0
+	for i in range(8):
+		get_node("l_cont/level_button" + str(i+1)).set_level(i + page * 8)
+	$page_num.text = str(page)
+
+func _on_page_minus_pressed():
+	set_page(page - 1)
+
+
+func _on_page_plus_pressed():
+	set_page(page + 1)
--- game/scripts/select_level.gd
+++ game/scripts/select_level.gd
@@ -1,34 +1,36 @@
 tool
 extends Control
 export var level = 0 setget set_level
-var global
+var global = null
 
 func set_level(value):
+	if global == null:
+		return
 	level = value
 	if has_node("level_num"):
 		$level_num.text = str(level)
-#	if has_node("level_button"):
-#		$level_button.text = "level " + str(level)
-
-func _ready():
-	global = get_node("/root/global")
-	if has_node("level_num"):
-		if !Engine.editor_hint:
-			$level_num.visible = false
+#	if has_node("level_num"):
+#		if !Engine.editor_hint:
+#			$level_num.visible = false
 	if has_node("prev"):
 		var has_prev = false
-		if level >= 0 and level < global.levels.size() :
+		if level >= 0 and level < global.levels.size():
 			var file2Check = File.new()
 			if file2Check.file_exists("res://prev/" + global.levels[level] + ".png"):
 				has_prev = true
 		if has_prev:
 			$prev.texture = load("res://prev/" + global.levels[level] + ".png")
+			$prev.visible = true
 		else:
 			$prev.visible = false
-			if !Engine.editor_hint:
-				visible = false
-			
-
+#			if !Engine.editor_hint:
+#				visible = false
+				
+func _ready():
+	global = get_node("/root/global")
+	set_level(level)
+	
 func _on_level_button_pressed():
-	global.set_level(level)
-	global.goto_scene("res://scenes/main.tscn")
+	if level >= 0 and level < global.levels.size():
+		global.set_level(level)
+		global.goto_scene("res://scenes/main.tscn")
